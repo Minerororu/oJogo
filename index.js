fc4aa90106da3a257bbd7941f59aea4cac38e227
Minerororu <pedro.dx.2008@gmail.com>
2024-05-07 17:47:09 -0300

    coisas interessantes express
    Co-authored-by: Eduardo Carvalho Valadão <eduardovaladao@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express')
+app = express()
+
+app.get('/', (req, res) => {
+    console.log(req.data)
+    res.send('Hello world')
+})
+
+app.listen(3000, () => {
+    console.log('to escutando a porta 3000')
+})
